2cde30e3fa6b9089e206e996dec3c896d259007f
Felipe Yui <felipe_m87@yahoo.com.br>
2021-05-06 23:36:00 +0930

    change avatar

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -63,7 +63,7 @@ export const AvatarInput = styled.div`
     height: 186px;
     border-radius: 50%;
   }
-  button {
+  label {
     position: absolute;
     width: 48px;
     height: 48px;
@@ -73,9 +73,13 @@ export const AvatarInput = styled.div`
     bottom: 0;
     border: 0;
     transition: background-color 0.2s;
+    cursor: pointer;
     display: flex;
     align-items: center;
     justify-content: center;
+    input {
+      display: none;
+    }
     svg {
       width: 20px;
       height: 20px;
