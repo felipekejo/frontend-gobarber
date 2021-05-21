157485e65d3ae2529fbe6745d97218cf58bcf321
Felipe Yui <felipe_m87@yahoo.com.br>
2021-05-22 00:39:58 +0930

    signup test with some issues  in the first test

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -34,7 +34,7 @@ const SignUp: React.FC = () => {
           name: Yup.string().required('Nome obrigatório'),
           email: Yup.string()
             .required('E-mail obrigatório')
-            .email('Digite um e-mailo valido'),
+            .email('Digite um e-mail valido'),
           password: Yup.string().min(6, 'No mínimo 6 dígitos'),
         });
         await schema.validate(data, {
